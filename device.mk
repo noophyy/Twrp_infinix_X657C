93252c042e0c91395260b49c6a1993eb23959f49
Miraclev1 <fathiralmsyah3@gmail.com>
2023-10-17 23:55:09 +0700

    first commit

---
+++ device.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/infinix/Max
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl-recovery
+    
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl.mt6761
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# MTK PlPath Utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils.recovery
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 29
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mtk.so \
+    fastbootd
+
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/infinix
---
+++ twrp_Max.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from spinel device
+$(call inherit-product, device/infinix/Max/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Max
+PRODUCT_NAME := twrp_Max
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix Max
+PRODUCT_MANUFACTURER := infinix
+PRODUCT_RELEASE_NAME := Infinix Infinix Max
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_x657c_h6117-user 10 QP1A.190711.020 286677 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X657C-GL/Infinix-X657C:10/QP1A.190711.020/GH-GL-220426V311:user/release-keys
