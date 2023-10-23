46cdda7be13236ffb34c33302d992f20d8803e0d
miracle ☘ <127039117+Miracleprjkt@users.noreply.github.com>
2023-10-23 22:10:21 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -46,10 +46,10 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
-# Fastbootd
+# fastboot/d hal
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mtk.so
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # Vendor ramdisk
 PRODUCT_COPY_FILES += \
