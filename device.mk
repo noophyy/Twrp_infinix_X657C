aaf0254448c395e66d1288687280a5f8dbb207da
miracle ☘ <127039117+Miracleprjkt@users.noreply.github.com>
2023-11-11 10:44:50 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -46,10 +46,10 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
-# fastbootd
+# Fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
 
 # OEM otacerts
 PRODUCT_EXTRA_RECOVERY_KEYS += \
