54483e98c6c3d3006f1525220fa43d3041a86f77
miracle ☘ <127039117+Miracleprjkt@users.noreply.github.com>
2023-11-12 14:28:21 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,19 @@
 #
 
 LOCAL_PATH := device/infinix/Max
+
+LOCAL_PATH := device/infinix/Max
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 29
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock 
+    android.hardware.fastboot@1.0-impl-mock.recovery
