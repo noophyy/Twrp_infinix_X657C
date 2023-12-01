9f3384c700198a3e1c214ecc0b0bd365a2a122c0
miracle ☘ <127039117+Miracleprjkt@users.noreply.github.com>
2023-12-01 19:44:12 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,26 +1,35 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+LOCAL_PATH := device/infinix/X657B
 
-LOCAL_PATH := device/infinix/Max
+PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl-recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl.mt6761
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 29
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd \
-    android.hardware.fastboot@1.0-impl-mock
+    android.hardware.fastboot@1.0-impl-mtk.so
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
